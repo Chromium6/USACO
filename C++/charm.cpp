3039870de23d8056aa73971e3c82c1cdf95666cf
TuringGear <agentmz24@gmail.com>
2018-03-03 18:54:30 -0800

    []

--- C++/charm.cpp
+++ C++/charm.cpp
@@ -13,14 +13,33 @@ LANG: C++
 using namespace std;
 #define LP(name, minVal, maxVal) for(int name = minVal; name < maxVal; name++)
 
+struct charm {
+    int w, d;
+
+    bool operator>(charm b) {
+        return d < b.d;
+    }
+};
+
 /* var dec */
+const string fileName = "test";
+int n; // total charms
+int m; // max weight
+charm *s; // store charm data
+
 /* func dec */
 
 int main() {
-    ifstream stdin ("test.in");
-    ofstream stdout ("test.out");
+    ifstream stdin (fileName + ".in");
+    ofstream stdout (fileName + ".out");
     /* init */
+    stdin >> n >> m;
+    s = new charm[n];
+    LP(i, 0, n)
+        stdin >> s[i].w >> s[i].d;
+    sort(s, s+n);
     /* run */
+    cout << s[0].w << " " << s[0].d;
     /* exit */
     cout << endl;
     stdin.close();
