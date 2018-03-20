0acadf430ca317cd477d79be4f2cfa17dfca66f0
Michael Zhang <agentmz24@gmail.com>
2018-03-20 11:10:57 -0700

    Regular Update

--- Java/Eclipse/USACO_Training/closing.java
+++ Java/Eclipse/USACO_Training/closing.java
@@ -1,18 +1,23 @@
 /**
 NAME: agentmz1
 LANG: JAVA
-PROG: test
+PROG: closing
 */
 
 import java.io.*;
 import java.util.*;
 
-public class className {
+public class closing {
 
     /* var dec */
     static String programName = "test";
+    static int n, m; // nodes and edges
+    static int[][] adj; // adjacency matrix
 
     /* func dec */
+    boolean connected() { // check if all connected
+        return true;
+    }
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader stdin = new BufferedReader(new FileReader(programName + ".in"));
@@ -20,6 +25,12 @@ public class className {
         PrintWriter stderr = new PrintWriter(System.err, true);
         StringTokenizer read = new StringTokenizer(stdin.readLine());
         /* init */
+        n = get(read, 0);
+        m = get(read, 0);
+        adj = new int[n][n];
+        for (int i = 0; i < m; i ++) {
+            
+        }
         /* run */
         /* exit */
         stdin.close();
--- Java/Eclipse/USACO_Training/sandcas.java
+++ Java/Eclipse/USACO_Training/sandcas.java
@@ -7,10 +7,15 @@ PROG: test
 import java.io.*;
 import java.util.*;
 
-public class className {
+public class sandcas {
 
     /* var dec */
     static String programName = "test";
+    static int n; // number of merlons
+    static int priceUp, priceDown; // change costs
+    static int costs;
+    static int[] a; // initial heights
+    static int[] b; // resulting heights
 
     /* func dec */
 
@@ -20,13 +25,30 @@ public class className {
         PrintWriter stderr = new PrintWriter(System.err, true);
         StringTokenizer read = new StringTokenizer(stdin.readLine());
         /* init */
+        n = get(read, 0);
+        priceUp =get(read, 0);
+        priceDown = get(read, 0);
+        costs = 0;
+        a = new int[n];
+        b = new int[n];
+        for (int i = 0; i < n; i ++) {
+            read = new StringTokenizer(stdin.readLine());
+            a[i] = get(read, 0);
+            b[i] = get(read, 0);
+        }
         /* run */
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int comp = 0; comp < n; comp ++) {
+            costs += Math.abs(a[comp]-b[comp])*(a[comp] > b[comp] ? priceDown : priceUp);
+        }
         /* exit */
+        stderr.println(costs);
         stdin.close();
         stdout.close();
         stderr.close();
-	}
-
+    }
+    
     static int get(StringTokenizer k, int l) {
         return Integer.parseInt(k.nextToken());
     }
